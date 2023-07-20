5c900bfbb6beeaa3414374d02d5b3850bb66ff12
Tom Kuson <mail@tjkuson.me>
2023-07-20 14:51:27 +0100

    Format

--- crates/ruff_linter/src/rules/pylint/rules/import_private_name.rs
+++ crates/ruff_linter/src/rules/pylint/rules/import_private_name.rs
@@ -1,6 +1,6 @@
-use rustpython_parser::ast::{Alias, Ranged, Stmt};
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
+use rustpython_parser::ast::{Alias, Ranged, Stmt};
 
 use crate::checkers::ast::Checker;
 
